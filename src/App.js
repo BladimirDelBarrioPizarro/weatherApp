bcf8cae8c65758c9a8dafd8abd714cdaca93f4aa
Bladi <bladidb@gmail.com>
2018-08-03 22:54:57 +0200

    Add components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WeatherLocation from './components/WeatherLocation';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+        </p>
+       <WeatherLocation></WeatherLocation>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Location.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Location = () => (
+  <div><h1>Las Rozas</h1></div>
+);
+
+export default Location;
