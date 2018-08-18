c7237bacf07217486cc1cb4d7ebacb872356206b
Bladi <bladidb@gmail.com>
2018-08-18 23:08:55 +0200

    Add renderForeCastItemDays

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -3,14 +3,30 @@ import PropTypes from 'prop-types';
 import './style/style.css';
 import ForecastItem from './ForecastItem/index';
 
+
+const days =[
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+    'Domingo'
+];
+
 class ForecastExtended extends Component{
+    
+    renderForeCastItemDays(){
+        return days.map ( day => (<ForecastItem weekDay={day}></ForecastItem>))
+    }
+
     render(){
         const {city} = this.props;
 
         return (
             <div >
                 <h3 className="forecast-title">Pronóstico extendido para {city}</h3>
-                <ForecastItem></ForecastItem>
+                {this.renderForeCastItemDays()}
            </div>
         )
     }
